c431c6a9fbbf3680a184cfd425f13eb061e9a5e1
Kevin North <knorth@huskers.unl.edu>
2015-03-29 17:44:49 -0500

    Added Ochiai algorithm
    I added the Ohciai suspiciousness calculation algorithm, which is very
    simple compared to the proximity-based weighting algorithm, to see if
    its behavior can help me diagnose the bug in the proximity-based
    weighting algorithm. The Ochiai algorithm works correctly, meaning that
    the dummy data I'm using is most likely correct.
    
    In addition, if I simply cannot get the proximity-based weighting
    algorithm to work, I can now fall back on using the Ochiai algorithm and
    still finish this project.

---
+++ Historical Fault Localization/src/edu/unl/knorth/historical_fault_localization/suspiciousness_calculation/ochiai/OchiaiSuspiciousnessCalculator.java
@@ -0,0 +1,44 @@
+package edu.unl.knorth.historical_fault_localization.suspiciousness_calculation.ochiai;
+
+import edu.unl.knorth.historical_fault_localization.intermediate_data.StatementData;
+import edu.unl.knorth.historical_fault_localization.intermediate_data.TestData;
+import edu.unl.knorth.historical_fault_localization.intermediate_data.TestExecutionData;
+import edu.unl.knorth.historical_fault_localization.suspiciousness_calculation.SuspiciousnessCalculator;
+import edu.unl.knorth.historical_fault_localization.suspiciousness_calculation.SuspiciousnessScore;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OchiaiSuspiciousnessCalculator extends SuspiciousnessCalculator {
+    @Override
+    public List<SuspiciousnessScore>
+        calculateSuspiciousness(TestExecutionData testExecutionData) {
+        List<SuspiciousnessScore> suspiciousnessScores = new ArrayList<>();
+            
+        int numFailingTests = testExecutionData.getTests(false).size();
+        
+        for(StatementData statement : testExecutionData.getStatements()) {
+            suspiciousnessScores.add(calculateIndividualSuspiciousness(
+                    testExecutionData, statement, numFailingTests));
+        }
+        
+        return suspiciousnessScores;
+    }
+
+    protected SuspiciousnessScore calculateIndividualSuspiciousness(
+        TestExecutionData data, StatementData statement, int numFailingTests) {
+        List<TestData> relevantFailingTests =
+                data.getTestsThatExecuteStatment(statement, false);
+        List<TestData> relevantPassingTests =
+                data.getTestsThatExecuteStatment(statement, true);
+        
+        int totalRelevantTests = relevantFailingTests.size()
+                + relevantPassingTests.size();
+        
+        double numerator = (double) relevantFailingTests.size();
+        double denominator = StrictMath.sqrt(((double)numFailingTests) *
+            ((double) totalRelevantTests));
+        double suspiciousness = numerator/denominator;
+        
+        return new SuspiciousnessScore(statement, suspiciousness);
+    }
+}
